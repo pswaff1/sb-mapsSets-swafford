3fd3b9c31d0c5b3dee3de54bd7ec22b57d8b4aae
Patrick Swafford <patrick.swafford@swaffordhouse.com>
2024-01-16 03:42:18 -0500

    QQ2 implemented

--- app.js
+++ app.js
@@ -2,4 +2,7 @@
 // Patrick Swafford
 
 // Quick Question 1
-const qq1 = () => (new Set([1,1,2,2,3,4])); // Returns {1,2,3,4}
+const qq1 = () => (new Set([1,1,2,2,3,4])) // Returns {1,2,3,4}
+
+// Quick Question 2
+const qq2 = () => ([...new Set("referee")].join("")) // Returns 'ref'
